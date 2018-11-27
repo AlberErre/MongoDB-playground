bfd583b88e5f353f294c94db538d996ec6dae1e2
Alber Erre <arblugo@gmail.com>
2018-11-27 17:34:17 +0000

    add update methods

--- createMovies.js
+++ createMovies.js
@@ -8,3 +8,12 @@ db.movies.find({name: "Harry Potter"})
 
 // more pretty!
 db.movies.find({name: "Harry Potter"}).pretty()
+
+// show all
+db.movies.find().pretty()
+
+// delete old register, and put this new info instead (dangerous, deletes everything else!)
+db.movies.update({name:"Harry Potter"}, {name: "harry potter and the philosopher's stone"})
+
+// add new info to current register
+db.movies.update({name:"Harry Potter"},{$set: {like: true}})
